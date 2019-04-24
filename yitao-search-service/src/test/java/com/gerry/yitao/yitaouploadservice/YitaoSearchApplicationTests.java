4d23a0ce74a9b482c4979e634606f8c24937a677
gerry <inet_ygssoftware@163.com>
2019-04-24 22:18:02 +0800

    易淘电商项目搜索服务后台逻辑完成

---
+++ yitao-search-service/src/test/java/com/gerry/yitao/yitaouploadservice/YitaoSearchApplicationTests.java
@@ -0,0 +1,11 @@
+package com.gerry.yitao.yitaouploadservice;
+
+import com.gerry.yitao.yitaosearchservice.YitaoSearchServiceApplication;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = YitaoSearchServiceApplication.class)
+public class YitaoSearchApplicationTests {
+}
---
+++ yitao-search-webapi/src/main/java/com/gerry/yitao/yitaosearchwebapi/YitaoUploadWebApiApplication.java
@@ -0,0 +1,16 @@
+package com.gerry.yitao.yitaosearchwebapi;
+
+import com.alibaba.dubbo.config.spring.context.annotation.EnableDubbo;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class,scanBasePackages = {"com.gerry.yitao"})
+@EnableDubbo
+public class YitaoUploadWebApiApplication {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(YitaoUploadWebApiApplication.class);
+    }
+}
